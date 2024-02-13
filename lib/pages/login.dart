46c4ef38432aa4569a7b0af7c3749438b3c791da
abdurrahman086 <rahmanabdur15689@gmail.com>
2024-02-13 17:59:06 +0700

    first commit

---
+++ lib/pages/login.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class MyLogin extends StatelessWidget {
+  const MyLogin({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+      child: Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+          begin: Alignment.topRight,
+          end: Alignment.bottomLeft,
+          colors: [
+            Colors.blue,
+            Colors.purple,
+            Colors.red,
+          ],
+        )),
+      ),
+    ));
+  }
+}
